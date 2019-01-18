a4e29dc5ac7ea4f8d07e90272f3929d438733642
abhi-kuks <46806402+abhi-kuks@users.noreply.github.com>
2019-01-18 14:50:51 +0530

    Data Preprocessing Template

---
+++ data_processing_template.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 13 14:08:07 2018
+
+@author: user
+"""
+#importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset=pd.read_csv("Data.csv")
+x=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,3].values
+ 
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,
+                                               random_state=0)
+"""from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.transform(x_test)"""
+
+
+
+
+
+
+
+
+
+
+
+
